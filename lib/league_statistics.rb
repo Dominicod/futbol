46daabdaa24e259e118d4fd9d94bcae0981c065a
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-03 16:09:36 -0600

    Refactor Nameable and implement into multiple classes

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -1,4 +1,5 @@
 require_relative 'nameable'
+
 class LeagueStatistics
   include Nameable
   def initialize(data)
--- lib/nameable.rb
+++ lib/nameable.rb
@@ -1,9 +1,5 @@
 module Nameable
   def self.return_team_name(team_id, data)
-    team_name = nil
-    data[:teams].each do |row|
-      team_name = row[:teamname] if row[:team_id] == team_id
-    end
-    team_name
+    team_name = (data[:teams].select {|row| row[:team_id] == team_id})[0][:teamname]
   end
 end
